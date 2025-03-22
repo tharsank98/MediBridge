8bf653319f90670d73a3a2017aa12f876fe56842
Tharsank98 <kanagathuraitharsan1998@gmail.com>
2025-03-22 14:32:40 +0530

    routes for get users edit users

---
+++ backend/controllers/userController.js
@@ -0,0 +1,121 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
+
+//Api to register user
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    //validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "enter a valid email" });
+    }
+
+    //validating strong password
+    if (password.length < 8) {
+      return res.json({ success: false, message: "enter a strong password" });
+    }
+
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Api for user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "user does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId).select("-password");
+
+    if (!userData) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    res.json({ success: true, userData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API to update user profile
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, phone, dob, address, gender } = req.body;
+    const imageFile = req.file;
+    if (!name || !phone || !dob || !address || !gender) {
+      return res.json({ success: false, message: "Data Missing" });
+    }
+
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      gender,
+      dob,
+      address: JSON.parse(address),
+    });
+
+    if (imageFile) {
+      //upload image to cloudinary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      });
+      const imageURL = imageUpload.secure_url;
+
+      await userModel.findByIdAndUpdate(userId, { image: imageURL });
+    }
+    res.json({ success: true, message: "profile updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getProfile, updateProfile };
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,37 @@
-import 'dotenv/config'
+import 'dotenv/config' // Load environment variables
 import express from 'express'
 import cors from 'cors'
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import adminRouter from './routes/adminRoute.js';
+import connectDB from './config/mongoDb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRouter.js'
 
 
-//app Config 
-const app = express()
-const port =process.env.PORT || 4001
-connectDB()
+// Initialize database and Cloudinary connections
+connectDB();
 connectCloudinary()
 
-//middlewares
-app.use(express.json())
-app.use(cors())
+// App configuration
+const app = express();
+const port = process.env.PORT || 4001
 
-//api endpoint
+// Middlewares
+app.use(express.json()); // Parse JSON request bodies
+app.use(cors()); // Enable CORS for all routes
 
-app.use('/api/admin',adminRouter)
-//localhost :4000/api/admin`
+// API endpoints
+app.use('/api/admin', adminRouter) // Admin routes
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
 
-app.get('/',(req,res)=>{
-   res.send('API is Working great ssss')
-})
 
-app.listen(port,()=>{
-    console.log("Server stated", port)
-})
+// Root endpoint
+app.get('/', (req, res) => {
+    res.send('API is working great!')
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
