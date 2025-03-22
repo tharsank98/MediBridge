e82a79ef433b54f6f5938b41e78e52c0e0588c28
Tharsank98 <kanagathuraitharsan1998@gmail.com>
2025-03-23 00:08:16 +0530

    user model changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,7 +13,6 @@ const userSchema = new mongoose.Schema({
   gender: { type: String, default: "Not Selected" },
   dob: { type: String, default: "Not Selected" },
   phone: { type: String, default: "0000000000" },
-  phone: { type: String, default: "0000000000" },
 });
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
